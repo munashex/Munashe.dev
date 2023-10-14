3f36b791dd45680b26be2a0b0eeaaef37c962099
munashex <munashemugondaa@gmail.com>
2023-10-14 18:00:41 +0200

    all done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,10 +18,10 @@ function Home() {
       <div className="gap-x-20 lg:py-2 lg:gap-x-28 mx-6 flex flex-col gap-y-11 items-center justify-center my-12 md:flex-row-reverse">
 
         <img src={munashe} alt="munashe"
-          className="w-64 lg:w-80 bg-gray-200 border-2 to-white-400 rounded-full border-black animate-fade-left"
+          className="w-64 lg:w-80 bg-gray-200 border-2 to-white-400 rounded-full border-black"
         />
 
-        <div className="flex flex-col w-[90%] md:w-[50%] lg:w-[40%] animate-fade-right  gap-y-5">
+        <div className="flex flex-col w-[90%] md:w-[50%] lg:w-[40%]  gap-y-5">
           <h1 className="text-4xl lg:text-5xl font-medium text-center">React Full Stack Developer</h1>
           <h1 className="text-lg text-slate-700 text-center">Greetings, I'm Munashe Mugonda, a fervent React Full Stack Programmer hailing from Africa.</h1>
 
@@ -32,7 +32,7 @@ function Home() {
         </div>
       </div>
 
-      <div className=" flex flex-col items-center gap-y-3 animate-fade-up">
+      <div className=" flex flex-col items-center gap-y-3">
         <h1 className="text-center text-xl font-bold">Tech Stack</h1>
         <div className="grid grid-cols-4 md:grid-cols-6 justify-items-center gap-3">
           {Logos.map((item) => (
